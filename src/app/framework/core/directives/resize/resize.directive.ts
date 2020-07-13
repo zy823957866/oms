bc193b871162e2243c08d4eeadc90af8b0144072
周勇 <zhouyong@dev.ithinkdt.com>
2020-07-13 16:39:49 +0800

    修改

--- src/app/framework/core/directives/resize/resize.directive.ts
+++ src/app/framework/core/directives/resize/resize.directive.ts
@@ -3,6 +3,12 @@ import { Directive, Input, ElementRef, Renderer2 } from '@angular/core';
 
 declare let $;
 
+const config = {
+    handleW: 4,         // 可拖拽区域离边界4px
+    divMinWith: 500,    // div最小宽度
+    divMinHeight: 300,  // div最小高度
+}
+
 @Directive({
     selector: '[elResize]'
 })
@@ -10,179 +16,136 @@ declare let $;
 export class ElResizeDirective {
     @Input() private elResize: boolean = true;
 
-    // 目标元素
-    target: HTMLElement;
-    // 宽度是否可以拖拽
-    resizeW: boolean = false;
-    // 高度是否可以拖拽
-    resizeH: boolean = false;
-    // 历史偏移X轴位置
-    startX: number;
-    // 历史偏移Y轴位置
-    startY: number;
-    // 最大偏移距离
-    size: number = 20;
-    // 最小偏移距离
-    minSize: number = 10;
-
     constructor(
         private el: ElementRef,
         private rend2: Renderer2
     ){}
 
     ngAfterViewInit(): void {
-        this.target = this.el.nativeElement.parentNode.parentNode.parentNode.parentNode;
-        this.dragDivResize(this.target);
-    }
+        // 计算box的宽高
+        setTimeout(() => {
+            // 设置拖拽改变div大小
+            this.dragDivResize();
 
-    dragDivResize(target: HTMLElement) {
-        let self = this;
-        $(target).on("mouseover mousemove", function($event) {
-            self.overHandler($event)
+            // 设置拖拽区域的相关尺寸
+            this.setDragSize();
         });
     }
 
-    overHandler(event) {
-        let self = this;
+    // 计算4个手柄初始宽高位置及外部div的宽高
+    setDragSize(){
+        let el = this.el.nativeElement;
+        let parentEl = this.el.nativeElement.parentNode.parentNode.parentNode;
+        let parentW = 2 * config.handleW + $(parentEl).width();
+        let parentH = 2 * config.handleW + $(parentEl).height();
 
-        // 设置拖拽的元素
-        let target = this.target;
+        // 外部div尺寸
+        $(el).width(parentW);
+        $(el).height(parentH);
 
-        // 获取位置
-        let scroll = this.getScrollOffsets();
-        
-        // 记录元素宽高
-        let w = $(target).width();
-        let h = $(target).height();
+        // 设置手柄
+        $(el).find('#left').height(parentH);
+        $(el).find('#top').width(parentW);
+        $(el).find('#right').height(parentH);
+        $(el).find('#bottom').width(parentW);
 
-        // 记录历史x、y轴偏移
-        this.startX = parseInt(event.clientX + scroll.x);
-        this.startY = parseInt(event.clientY + scroll.y);
+        setTimeout(() => {
+            $(el).css({
+                position: 'absolute',
+                left: ($(document).width() - parentW) / 2 + 4 + 'px',
+                top: ($(document).height() - parentH) / 2 - 1 + 'px',
+            })
+        },1000);
+    }
 
-        // 手柄位置
-        let endX = target.offsetLeft + w - this.startX;
-        let endY = target.offsetTop + h - this.startY;
+    dragDivResize() {
+        let u_left = document.getElementById('left');
+        let u_top = document.getElementById('top');
+        let u_right = document.getElementById('right');
+        let u_bottom = document.getElementById('bottom');
 
-        if( (0 < endX && endX < this.size) || ( 0 < endY && endY < this.size )) {
-            target.style.outline = "2px dashed #333";
+        let currentX = 0,               // 用来记录鼠标按下时的横坐标
+            currentY = 0,               // 用来记录鼠标按下时的纵坐标
+            currentDivWidth = 0,        // 鼠标按下时div的宽度
+            currentDivHeight = 0,       // 鼠标按下时div的高度
+            currentDivX = 0,            // 鼠标按下时div的横坐标
+            currentDivY = 0;            // 鼠标按下时div的纵坐标
+        let currentCanvasDiv = null;    // 鼠标按下时存放要改变大小的div
+        let currentBorder = null;       // 鼠标按下时存放当前拖动的边
 
-            if(( 0 > endX || endX > this.size ) && 0 < endY && endY < this.size ) {
-                this.resizeW = false;
-                this.resizeH = true;
-                document.body.style.cursor = "s-resize";
+        // 绑定鼠标按下事件
+        u_left.onmousedown = mouseDown;
+        u_top.onmousedown = mouseDown;
+        u_right.onmousedown = mouseDown;
+        u_bottom.onmousedown = mouseDown;
+
+        function mouseDown(event) {
+            currentX = event.clientX;
+            currentY = event.clientY;
+            const path = event.path || (event.composedPath && event.composedPath()); // 获取当前点击事件冒泡所经过的路径同时解决火狐的兼容问题
+            currentCanvasDiv = path[1];
+            currentBorder = path[0].id;
+
+            currentDivWidth = parseInt(currentCanvasDiv.style.width);
+            currentDivHeight = parseInt(currentCanvasDiv.style.height);
+
+            console.log(currentCanvasDiv, currentDivWidth, currentDivHeight)
+
+            currentDivX = parseInt(currentCanvasDiv.style.left);
+            currentDivY = parseInt(currentCanvasDiv.style.top);
+
+            document.addEventListener("mousemove", eval(currentBorder + 'Move'));
+            document.addEventListener("mouseup", mouseUp);
+        }
+
+        function mouseUp() {
+            document.removeEventListener("mousemove", eval(currentBorder + 'Move'));
+            document.removeEventListener("mouseup", mouseUp);
+        }
+
+        // 拖动左侧边
+        function leftMove(event) {
+            let _X = event.clientX;
+            let _width = currentDivWidth + currentX - _X; // 拖动后div的宽度
+            if (_width <= config.divMinWith) { // 设置可以拖动的最小宽度
+                u_bottom.style.width = u_top.style.width = currentCanvasDiv.style.width = config.divMinWith + 'px';
+                currentCanvasDiv.style.left = currentDivWidth + currentDivX - config.divMinWith + 'px'; // 使左侧边向左移动，否则改变宽度时div会默认向右增宽
+            } else {
+                u_bottom.style.width = u_top.style.width = currentCanvasDiv.style.width = _width + 'px';
+                currentCanvasDiv.style.left = currentDivX + _X - currentX + 'px';
             }
+        }
 
-            if( 0 < endX && endX < this.size && ( 0 > endY || endY > this.size ) ){
-                this.resizeW = true;
-                this.resizeH = false;
-                document.body.style.cursor = "w-resize";
+        function rightMove(event) {
+            let _X = event.clientX;
+            let _width = currentDivWidth + _X - currentX;
+            if (_width <= config.divMinWith) {
+                u_bottom.style.width = u_top.style.width = currentCanvasDiv.style.width = config.divMinWith + 'px';
+            } else {
+                u_bottom.style.width = u_top.style.width = currentCanvasDiv.style.width = _width + 'px';
             }
+        }
 
-            if( 0 < endX && endX < this.size && 0 < endY && endY < this.size ) {
-                this.resizeW = true;
-                this.resizeH = true;
-                document.body.style.cursor = "se-resize";
+        function bottomMove(event) {
+            let _Y = event.clientY;
+            let _height = currentDivHeight + _Y - currentY;
+            if (_height <= config.divMinHeight) {
+                u_left.style.height = u_right.style.height = currentCanvasDiv.style.height = config.divMinHeight + 'px';
+            } else {
+                u_left.style.height = u_right.style.height = currentCanvasDiv.style.height = _height + 'px';
             }
-
-            $(target).on('mousedown', function($event) { self.downHandler( $event, target ) });
-        } else {
-            this.resizeW = false;
-            this.resizeH = false;
-
-            $(target).off('mousedown', function($event) { self.downHandler( $event, target ) });
-        }
-    }
-
-    // 按下手柄
-    downHandler(event, target) {
-        let self = this;
-        console.log('sd--sf-d-sf--d-f-')
-        if (document.addEventListener) {
-            
-            document.addEventListener("mousemove", function($event) { self.moveHandler($event, target ) }, true);
-            document.addEventListener("mouseup", function($event) { self.upHandler($event, target ) }, true);
-        } else if (document['attachEvent']) {
-            target.setCapture();
-            target.attachEvent("onlosecapeture", function($event) { self.upHandler($event, target ) });
-            target.attachEvent("onmouseup", function($event) { self.upHandler($event, target ) });
-            target.attachEvent("onmousemove", function($event) { self.moveHandler($event, target ) });
-        }
-    }
-
-    
-    // 按下鼠标并进行移动手柄
-    moveHandler(event, target) {
-        if (target == document.body) { return; }
-
-        console.log("s-s-s-s-s-s--s-s-")
-
-        event = event || window.event;
-
-        let deltaX, deltaY;
-        let scroll = this.getScrollOffsets();
-        
-        // 当前手柄所在的X、Y轴左边
-        let currentX = parseInt(event.clientX + scroll.x);
-        let currentY = parseInt(event.clientY + scroll.y);
-        if (this.resizeW) {
-            deltaX = currentX - this.startX;
-            target.style.width = (($(target).width() + deltaX < this.minSize) ? this.minSize : ($(target).width() + deltaX)) + 'px';
-            this.startX = currentX;
         }
 
-        if(this.resizeH) {
-            deltaY = currentY - this.startY;
-            target.style.height = (($(target).height() + deltaY < this.minSize) ? this.minSize : ($(target).height() + deltaY)) + 'px';
-            this.startY = currentY;
+        function topMove(event) {
+            let _Y = event.clientY;
+            let _height = currentDivHeight + currentY - _Y;
+            if (_height <= config.divMinHeight) {
+                u_left.style.height = u_right.style.height = currentCanvasDiv.style.height = config.divMinHeight + 'px';
+                currentCanvasDiv.style.top = currentDivHeight + currentDivY - config.divMinHeight + 'px';
+            } else {
+                u_left.style.height = u_right.style.height = currentCanvasDiv.style.height = _height + 'px';
+                currentCanvasDiv.style.top = currentDivY + _Y - currentY + 'px';
+            }
         }
-
-        event.stopPropagation ? event.stopPropagation() : event.cancelBubble = true;
-    }
-
-
-    // 停止移动手柄
-    upHandler(event, target) {
-        event = event || window.event;
-
-        let self = this;
-
-        // 停止拖拽手柄
-        this.resizeW = false;
-        this.resizeH = false;
-
-        $(target).on("mouseout", function($event){ self.outHandler($event, target) });
-
-        if (document.removeEventListener) {
-            document.removeEventListener("mousemove", function($event) { self.moveHandler($event, target ) }, true);
-            document.removeEventListener("mouseup", function($event) { self.upHandler($event, target ) }, true);
-        } else if (document['detachEvent']) {
-            target.detachEvent("onlosecapeture", function($event) { self.upHandler($event, target ) });
-            target.detachEvent("onmouseup", function($event) { self.upHandler($event, target ) });
-            target.detachEvent("onmousemove", function($event) { self.moveHandler($event, target ) });
-            target.releaseCapture();
-          }
-
-        event.stopPropagation ? event.stopPropagation() : event.cancelBubble = true;
-    }
-
-    // 还原拖拽效果
-    outHandler(event, target) {
-        target.style.outline = "none";
-        document.body.style.cursor = "default";
-    }
-
-    // 获取元素的偏移量
-    getScrollOffsets(w: any = null) {
-        w = w || window;
-
-        if (w.pageXOffset != null) {
-            return { x: w.pageXOffset, y: w.pageYOffset };
-        }
-        var d = w.document;
-        if (document.compatMode == "CSS1Compat") {
-            return { x: d.documentElement.scrollLeft, y: d.documentElement.scrollTop };
-        }
-
-        return { x: d.body.scrollLeft, y: d.body.scrollTop };
     }
 }
